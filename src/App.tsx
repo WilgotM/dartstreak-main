26158e7e7de1cb06cad20a4bf714f6a7fd706ed1
WilgotM <wilgot10@yahoo.com>
2026-02-11 01:27:32 +0100

    chore: enable browser router and update sitemap with static pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Index from "./pages/Index";
@@ -30,7 +30,7 @@ const App = () => {
             <TooltipProvider>
               <Toaster />
               <Sonner />
-              <HashRouter>
+              <BrowserRouter>
                 <UsernameGuard>
                   <Routes>
                     <Route path="/" element={<Index />} />
@@ -50,7 +50,7 @@ const App = () => {
                 </UsernameGuard>
                 <InstallPrompt />
                 <IOSInstallReminder />
-              </HashRouter>
+              </BrowserRouter>
             </TooltipProvider>
           </AuthProvider>
         </QueryClientProvider>
